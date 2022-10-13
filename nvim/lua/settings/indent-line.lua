a30798694f0339840b98f2f781afc3b6c8d88241
zouqiang001 <zouqiang001@ke.com>
2022-10-13 10:58:15 +0800

    update

--- nvim/lua/settings/indent-line.lua
+++ nvim/lua/settings/indent-line.lua
@@ -5,8 +5,8 @@ vim.opt.list = true
 
 require("indent_blankline").setup {
     --space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+    --show_current_context = true,
+    --show_current_context_start = true,
     --show_end_of_line = true,
 }
 
