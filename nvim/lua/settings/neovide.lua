d1e0e22bdaed7e1b1f263dd0807d9d47f2dd5bd3
zouqiang001 <zouqiang001@ke.com>
2022-10-13 15:23:15 +0800

    add neovide

---
+++ nvim/lua/settings/neovide.lua
@@ -0,0 +1,9 @@
+if vim.fn.exists(vim.g.neovide) then
+    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_confirm_quit = 1
+    vim.g.neovide_remember_window_size = 1
+    vim.g.neovide_cursor_animation_length = 0.13
+    vim.g.neovide_cursor_trail_size = 0.8
+    vim.g.neovide_cursor_antialiasing = 1
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+end
